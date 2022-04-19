b253065e393853ad45186051b23ab5f982320948
Matherss <mathers_beats@mail.ru>
2022-04-19 12:23:48 +0300

    fixed:bug in register

--- engine/register.php
+++ engine/register.php
@@ -7,7 +7,7 @@ $user_name = $_POST['name'] ? strip_tags($_POST['name']) : '';
 // $salt = strrev(md5($login));  - Пока что отключил за ненадобностью, но не удалил, чтобы Вы видели, что я в курсе :)
 // $pass = $salt.md5($password);
 $pass = md5($user_password);
-$sql = "INSERT INTO `user`(`user_name`, `user_login`, `user_password`,`is_admin`) VALUES ('$user_name','$user_login','$user_password','0')";
+$sql = "INSERT INTO `user`(`user_name`, `user_login`, `user_password`,`is_admin`) VALUES ('$user_name','$user_login','$pass','0')";
 
 $res = mysqli_query($connect,$sql) or die("Error".mysqli_error($connect));
 
